7b78b3b230c7cb777c7d034002ccdf2a05a396f3
bgchoi205 <choibg205@naver.com>
2021-06-29 09:41:19 +0900

    로직분리

--- src/com/cbg/exam/app/controller/UsrArticleController.java
+++ src/com/cbg/exam/app/controller/UsrArticleController.java
@@ -30,84 +30,114 @@ public class UsrArticleController {
 	public void performAction(Rq rq) {
 		
 		if(rq.getActionPath().equals("/usr/article/write")) {
-			
-			Article article = new Article();
-			
-			System.out.printf("제목 입력 : ");
-			String title = sc.nextLine().trim();
-			System.out.printf("내용 입력 : ");
-			String body = sc.nextLine().trim();
-			
-			lastArticleId++;
-			
-			article.id = lastArticleId;
-			article.regDate = Util.getNow();
-			article.updateDate = Util.getNow();
-			article.title = title;
-			article.body = body;
-			
-			articles.add(article);
-			
-			System.out.println(article.id + "번 게시물 생성완료");
-			
+			actionWrite(rq);
 		}
 		else if(rq.getActionPath().equals("/usr/article/list")) {
-			
-			for(int i = (articles.size() - 1); i >= 0; i--) {
-				System.out.println(articles.get(i));
-			}
-			
+			actionList(rq);
 		}
 		else if(rq.getActionPath().equals("/usr/article/detail")) {
-			int id = rq.getIntParam("id", 0);
-			
-			Article article = getArticleById(id, articles);
-			
-			if(article == null) {
-				System.out.println("존재하지 않는 게시물입니다.");
-				return;
-			}
-			System.out.println("번호:" + article.id);
-			System.out.println("등록:" + article.regDate);
-			System.out.println("수정:" + article.updateDate);
-			System.out.println("제목:" + article.title);
-			System.out.println("내용:" + article.body);
+			actionDetail(rq);
 		} else if(rq.getActionPath().equals("/usr/article/delete")) {
-			int id = rq.getIntParam("id", 0);
-			
-			Article article = getArticleById(id, articles);
-			
-			if(article == null) {
-				System.out.println("존재하지 않는 게시물입니다.");
-				return;
-			}
-			articles.remove(article);
-			
-			System.out.println(article.id + "번 게시물 삭제완료");
-			
+			actionDelete(rq);
 		} else if(rq.getActionPath().equals("/usr/article/modify")) {
-			int id = rq.getIntParam("id", 0);
-			
-			Article article = getArticleById(id, articles);
-			
-			if(article == null) {
-				System.out.println("존재하지 않는 게시물입니다.");
-				return;
-			}
-			System.out.printf("제목 입력 : ");
-			String title = sc.nextLine().trim();
-			System.out.printf("내용 입력 : ");
-			String body = sc.nextLine().trim();
-			
-			article.title = title;
-			article.body = body;
-			article.updateDate = Util.getNow();
-			
-			System.out.println(article.id + "번 게시물 수정 완료");
+			actionModify(rq);
 		}
 		
 	}
 	
+	private void actionModify(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+		
+		Article article = getArticleById(id, articles);
+		
+		if(article == null) {
+			System.out.println("존재하지 않는 게시물입니다.");
+			return;
+		}
+		System.out.printf("제목 입력 : ");
+		String title = sc.nextLine().trim();
+		System.out.printf("내용 입력 : ");
+		String body = sc.nextLine().trim();
+		
+		article.title = title;
+		article.body = body;
+		article.updateDate = Util.getNow();
+		
+		System.out.println(article.id + "번 게시물 수정 완료");
+	}
+
+
+
+
+	private void actionDelete(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+		
+		Article article = getArticleById(id, articles);
+		
+		if(article == null) {
+			System.out.println("존재하지 않는 게시물입니다.");
+			return;
+		}
+		articles.remove(article);
+		
+		System.out.println(article.id + "번 게시물 삭제완료");
+	}
+
+
+
+
+	private void actionDetail(Rq rq) {
+		int id = rq.getIntParam("id", 0);
+		
+		Article article = getArticleById(id, articles);
+		
+		if(article == null) {
+			System.out.println("존재하지 않는 게시물입니다.");
+			return;
+		}
+		System.out.println("번호:" + article.id);
+		System.out.println("등록:" + article.regDate);
+		System.out.println("수정:" + article.updateDate);
+		System.out.println("제목:" + article.title);
+		System.out.println("내용:" + article.body);
+	}
+
+
+
+
+	private void actionList(Rq rq) {
+		for(int i = (articles.size() - 1); i >= 0; i--) {
+			System.out.println(articles.get(i));
+		}
+	}
+
+
+
+
+	private void actionWrite(Rq rq) {
+		Article article = new Article();
+		
+		System.out.printf("제목 입력 : ");
+		String title = sc.nextLine().trim();
+		System.out.printf("내용 입력 : ");
+		String body = sc.nextLine().trim();
+		
+		lastArticleId++;
+		
+		article.id = lastArticleId;
+		article.regDate = Util.getNow();
+		article.updateDate = Util.getNow();
+		article.title = title;
+		article.body = body;
+		
+		articles.add(article);
+		
+		System.out.println(article.id + "번 게시물 생성완료");
+	}
+
+
+
+
 	private void makeTestData() {
 		for(int i = 1; i <= 10; i++) {
 			Article article = new Article();
